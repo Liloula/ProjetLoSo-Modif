e5afed07ede4a97a514d083cc97dc296252e98b1
Unknown <eloise.hall@sfr.fr>
2018-03-14 22:52:31 +0100

    47 4:28

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+/*show and hide menu */
+
+$(document).ready(function(){
+
+'use strict';
+
+$(window).scroll(function(){
+
+  'use strict';
+
+  if($(window).scrollTop() < 80 ){
+
+    $('navbar').css ({
+      'margin-top':'-100px',
+      'opacity':'0'
+
+    });
+
+  }else{
+    $('navbar').css ({
+      'margin-top':'0px',
+      'opacity': '1'
+
+    });
+  }
+
+
+  });
+
+
+});
